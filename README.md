502e4392044cde3e31dc9eae9541577ed51f0a96
Neruthes <i@neruthes.xyz>
2022-12-11 20:43:29 +0000

    Init

---
+++ README.md
@@ -0,0 +1,7 @@
+# Neruthes Homepage Gen3
+
+Experimental deployment: [https://homepage-gen3.neruthes.xyz/](https://homepage-gen3.neruthes.xyz/).
+
+## Copyright
+
+Copyright (c) 2012-2022 Neruthes. All rights reserved.
---
+++ build.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+function rebuild_all_tex_files() {
+    for TEX_FILE_PATH in $(find -path '*/*.tex' | grep -v TMPL | sort); do
+        PDF_FILE_PATH="_dist/${TEX_FILE_PATH:2:-4}.pdf"
+        if [[ ! -e $PDF_FILE_PATH ]]; then
+            echo "Artifact does not exist!"
+            ntex "$TEX_FILE_PATH" --2
+        else
+            if [[ $(date +%s -r $TEX_FILE_PATH) -gt $(date +%s -r $PDF_FILE_PATH) ]]; then
+                echo "Must rebuild this file!"
+                ntex "$TEX_FILE_PATH"
+            fi
+        fi
+    done
+}
+
+function make_indexhtml_for_dirs() {
+    INDEXABLE_DIRS_LIST="articles"
+    for RAWDIR in $INDEXABLE_DIRS_LIST; do
+        DIR="wwwdist/$RAWDIR"
+        INDEXFILE="$DIR/index.html"
+        sed "s:HTMLTITLE:Neruthes | ${DIR^^}:" .src/dirindex.head.html > $INDEXFILE
+        for ITEM in $(ls $DIR | grep -v 'index.html' | sort); do
+            echo "<a class='dirindexlistanchor' href='./$ITEM'>$ITEM</a>" >> $INDEXFILE
+        done
+        cat .src/dirindex.tail.html >> $INDEXFILE
+    done
+}
+
+
+
+
+
+
+case $1 in
+    1)
+        rsync -av --delete wwwsrc/ wwwdist/
+        ;;
+    2)
+        rebuild_all_tex_files
+        ;;
+    3)
+        rsync -av _dist/ wwwdist/ --exclude tex-tmp
+        ;;
+    4)
+        make_indexhtml_for_dirs
+        ;;
+    *|full)
+        bash build.sh 1
+        bash build.sh 2
+        bash build.sh 3
+        bash build.sh 4
+        ;;
+esac
