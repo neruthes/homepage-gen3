c799d5a80853fa158c859998ec1d6e3fffb84553
Neruthes <i@neruthes.xyz>
2023-02-03 19:27:35 +0000

    Automatic deploy command: 2023-02-03 19:27:35

--- .data/articles/makelist.sh
+++ .data/articles/makelist.sh
@@ -11,13 +11,13 @@ for VOL in $VOL_LIST; do
     ### Reset list
     rm "$LISTFN" "$LISTFN.draft" 2>/dev/null
     ### Build list for live document
-    for TEXFILE in $(ls .data/articles/vol$VOL/*.TEX | sort); do
-        echo '\input{'"$TEXFILE"'}' >> $LISTFN
-    done
+    find .data/articles/vol$VOL -name '*.TEX' | sort | while read -r TEXFILE; do
+        echo '\input{'"$TEXFILE"'}'
+    done >> $LISTFN
     ### Build list for draft document
-    for TEXFILE in $(ls .data/articles/vol$VOL/* | sort); do
-        echo '\input{'"$TEXFILE"'}' >> $LISTFN.draft
-    done
+    find .data/articles/vol$VOL -iname '*.TEX' | sort | while read -r TEXFILE; do
+        echo '\input{'"$TEXFILE"'}'
+    done >> $LISTFN.draft
     ### Let the draft follow the live
     sed "s|list-$VOL.TEX|list-$VOL.TEX.draft|" articles/Neruthes_articles_vol$VOL.tex > $DRAFTDIR/Neruthes_articles_vol$VOL-draft.tex
 done
